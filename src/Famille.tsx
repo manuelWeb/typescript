9250a8e13dd5596355265fd762c5959bd37ecc3c
Becquet <mdevries@domoti.fr>
2019-10-23 20:06:21 +0200

    ajout enfants sur IFamille

--- src/Famille.tsx
+++ src/Famille.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { father, famille } from './familleObj'
+import { famille } from './familleObj'
 
 export interface IFamille {
   famille?: IFamille
@@ -7,13 +7,20 @@ export interface IFamille {
 }
 
 export default class Famille extends React.Component<IFamille> {
-
+  f1Length = famille.famil1.enfants.length
   public render(): JSX.Element {
     return (
       <div className="famille">
-        Olã famille {famille ? famille.name : 'Pas de nom?'}
+        Olã famille {famille ? famille.famil1.name : 'Pas de nom?'}
         <br />
-        Donc le père: {father.name}
+        Donc le père {famille.famil1.father.firstName} {famille.famil1.name} {famille.famil1.father.age} ans <br />
+        à {this.f1Length} enfants :
+        {famille.famil1.enfants.map((child, idx) =>
+          <span key={idx}> {child.name}{idx + 1 === this.f1Length && '.'}{idx + 1 < this.f1Length - 1 ? ',' : idx + 1 === this.f1Length - 1 ? ' et' : null}
+          </span>)}
+        <br />
+        {/* https://medium.com/hackernoon/conditional-javascript-for-experts-d2aa456ef67c */}
+        {(this.f1Length > 1 && 'Plusieurs enfants') || 'Un seul enfant'}
       </div>
     )
   }
--- src/familleObj.ts
+++ src/familleObj.ts
@@ -1,11 +1,24 @@
 // import IFamille from './Famille'
+const Nick = { name: 'Nick', age: 14 }
+const Estelle = { name: 'Estelle', age: 10 }
+const Mickey = { name: 'Mickey', age: 28 }
+const Olfa = { name: 'Olfa', age: 28 }
 
-export const father = {
-  name: 'John',
-  age: 48,
-  // enfants: Nick
+const famil1 = {
+  name: 'Doe',
+  // enfants: [Nick]
+  enfants: [Nick, Estelle, Mickey, Olfa]
 }
 
 export const famille = {
-  name: 'Doe'
+  famil1: {
+    father: { firstName: 'John', age: 48 },
+    name: famil1.name,
+    enfants: famil1.enfants
+  }
 }
+
+
+
+
+
